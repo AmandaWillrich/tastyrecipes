e31c10b48efecbf225270a4d38670066cd4e120f
Amanda Willrich Fuchs <111703865+AmandaWillrich@users.noreply.github.com>
2023-03-10 15:43:33 -0300

    changing models

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blog/inicio.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/inicio.html', context)
 
 
 def recipe(request):
